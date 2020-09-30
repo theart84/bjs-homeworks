ca2ef6ef7d1b4b01022258d0537d1d8a64a9fd07
theart84 <artshadow74@gmail.com>
2020-09-30 14:13:58 +0500

    Решение домашнего задания 1.1 «Базовый синтаксис»

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -5,23 +5,19 @@
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
 
-function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+function averageMark(algebra, geography, physics) {	
+    let averageRating = (algebra + geography + physics) / 3;    
+	return averageRating;
 }
-
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let message = `Привет, мир! Меня зовут ${userName}`;
+	return message;
 }
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;	
+    return result;
 }
