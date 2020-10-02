d21ac02a13dfb1ca759bc60beba7f4b5ba8cff96
theart84 <artshadow74@gmail.com>
2020-10-02 20:14:14 +0500

    Решение задания 1.2 «Расширенный синтаксис»

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,37 @@
+'use strict';
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let array = [];
+    let discriminant = b**2 - 4 * a * c;
+    if (discriminant > 0){
+        array =  [(- b + Math.sqrt(discriminant)) / (2 * a), (- b - Math.sqrt(discriminant)) / (2 * a)];
+    } else if (discriminant === 0) {
+        array = [- b / (2 * a)];
+    }
+    return array;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let arr = [];
+    if (marks.length === 0) {
+        return 0;
+    } else if (marks.length > 5) {
+        console.log(`Количество оценок больше пяти!`);
+        arr = marks.slice(0, 5);
+    } else {
+        arr = marks.slice();
+    }
+    return (arr.reduce((curValue, prevValue) => {
+        return curValue + prevValue
+    }, 0)) / arr.length;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let yearPerson = dateOfBirthday.getFullYear();
+    let currentYear = new Date().getFullYear();
+    if ((currentYear - yearPerson) >= 18) {
+        return `Не желаете ли олд-фэшн, ${name}?`
+    } else {
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
 }
