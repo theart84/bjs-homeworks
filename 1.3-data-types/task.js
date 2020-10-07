d99ac3d285518dc6f0e805cd77a2cad7e788309a
theart84 <artshadow74@gmail.com>
2020-10-07 15:14:37 +0500

    Решение задания 1.3 «Типы данных».

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,30 @@
+'use strict'
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${window.percent.value}"`
+  } else if (isNaN(contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${window.contribution.value}"`
+  } else if (isNaN(amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${window.amount.value}"`
+  }
+  let currentMonth = new Date().getMonth();
+  let currentYear = new Date().getFullYear();
+  let yearsBetweenYears = date.getFullYear() - currentYear;
+  let amountMonth = yearsBetweenYears * 12 - currentMonth + date.getMonth();
+  let interestRate = (percent / 100) / 12;
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (interestRate + (interestRate / (((1 + interestRate) ** amountMonth) - 1)))
+  let totalAmount = (amount + (monthlyPayment * amountMonth - amount)).toFixed(2);
+
+  console.log(Number(totalAmount));
+
+  return Number(totalAmount);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  if (!name) {
+    return `Привет, мир! Меня зовут Аноним`
+  }
+  return `Привет, мир! Меня зовут ${name}`;
 }
