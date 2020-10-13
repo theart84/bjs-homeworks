9c2fe7860e611327c32df6323214228309f1b186
theart84 <artshadow74@gmail.com>
2020-10-13 16:42:49 +0500

    Решение задания 2.2 «Прототип и конструктор объекта»

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,32 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict'
+String.prototype.isPalindrome = function () {
+  const newString = this.replace(/\s/g, '').toLocaleLowerCase();
+  for (let i = 0; i < newString.length; i++) {
+    if (newString[i] !== newString[newString.length - 1 - [i]]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  if (marks.length === 0)
+    return 0;
+  const average = marks.reduce((curValue, prevValue) => {
+    return curValue + prevValue
+  }, 0) / marks.length;
+  return Math.round(average);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthday);
+  let agePerson = currentDate.getFullYear() - birthdayDate.getFullYear();
+  let diffCurrentDate = currentDate.setFullYear(1970);
+  let diffBirthdayDate = birthdayDate.setFullYear(1970);
+  console.log(diffCurrentDate)
+  console.log(diffBirthdayDate)
+  if (diffCurrentDate < diffBirthdayDate)
+     agePerson -= 1
+  return agePerson >= 18;
 }
